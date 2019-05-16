d08efbeaecca7355e4316a4f57ae8607d566357d
Gabriel Stellini <gabriel.stellini@ozan.com>
2019-05-16 18:07:20 +0200

    Add types

---
+++ src/schema/typedefs.js
@@ -0,0 +1,30 @@
+const { gql } = require("apollo-server/dist/index");
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+   type Book {
+    id: ID!
+    title: String!
+    author: String!
+    isbn: String!
+    url: String!
+  }
+
+  type Mutation {
+    createBook(title: String!, author: String!): Book
+  }
+
+  type Query {
+    allBooks(filter: bookFilter): [Book!]!
+  }
+  
+  input bookFilter {
+    id: ID
+    title: String
+    author: String
+    isbn: String
+    url: String
+  }
+`;
+
+module.exports = typeDefs;
